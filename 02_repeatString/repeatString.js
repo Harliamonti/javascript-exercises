a01d9c406146b7bd275ea278e4b3907588cb54b3
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-14 10:49:51 +0200

    Finish repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, iterations) {
+    let result = "";
+    
+    if (iterations < 0) return "ERROR";
+    else {
+        for (let i = 0; i < iterations; i++) {
+            result += string;    
+        }
+        
+        return result;
+    }
 };
 
 // Do not edit below this line
