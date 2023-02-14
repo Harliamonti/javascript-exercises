fc6bc56b18f8d3f449a8a2566932efb484487eb2
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-14 14:01:23 +0200

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeNums) {
+    for (const removeNum of removeNums) {
+        array = array.filter(num => num !== removeNum);
+    }
+    return array;
 };
 
+// removeFromArray([1, 2, 34, 4, 55, 4], 3, 4);
 // Do not edit below this line
 module.exports = removeFromArray;
